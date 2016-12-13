df40c64b632afb0cb72b18d3d40b8462efa75c3e
mattgoorley <mattgoorley@gmail.com>
2016-12-13 15:25:33 -0500

    upload to github

--- app/app.py
+++ app/app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
+@app.route('/hello')
+def hello():
     return "Hello Matt's World"
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
